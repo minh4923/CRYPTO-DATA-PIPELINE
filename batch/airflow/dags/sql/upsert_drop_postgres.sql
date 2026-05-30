dc258ffcc337f0da04984a3e72300a873fd09d13
Tnminh <minh03679123297@gmail.com>
2026-05-30 22:26:18 +0700

    fix(batch): update upsert and drop logic for postgres

--- batch/airflow/dags/sql/upsert_drop_postgres.sql
+++ batch/airflow/dags/sql/upsert_drop_postgres.sql
@@ -1,3 +1,15 @@
+CREATE SCHEMA IF NOT EXISTS core;
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE IF NOT EXISTS core.{{ var.json.bybit.product | lower }} (
+    timestamp TIMESTAMP PRIMARY KEY,
+    open NUMERIC,
+    high NUMERIC,
+    low NUMERIC,
+    close NUMERIC,
+    volume NUMERIC
+);
+
 INSERT INTO core.{{ var.json.bybit.product }} (timestamp, open, high, low, close, volume)
 SELECT timestamp, open, high, low, close, volume FROM staging.{{ var.json.bybit.product }}{{ ds_nodash }}
 ON CONFLICT(timestamp) DO UPDATE SET
